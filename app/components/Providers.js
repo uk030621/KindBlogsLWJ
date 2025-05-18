3cc2dca8a8489329eeea7b619cc54f947d5dad63
uk030621 <123797901+uk030621@users.noreply.github.com>
2025-05-18 10:19:45 +0100

    Increases footer vertical width so it shows up better a base on mobile screen

--- app/components/Providers.js
+++ app/components/Providers.js
@@ -9,7 +9,7 @@ export default function Providers({ children, session }) {
       <div className="min-h-screen flex flex-col bg-background">
         <Navbar />
         <main className="flex-grow">{children}</main>
-        <footer className="bg-slate-200 text-black text-center py-4">
+        <footer className="bg-slate-200 text-black text-center py-4 pb-8">
           © {new Date().getFullYear()} LWJ Helpful Posts. All rights reserved.
         </footer>
       </div>
