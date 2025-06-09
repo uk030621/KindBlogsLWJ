bc773b816f98623ff560d8f93155489500118e86
uk030621 <123797901+uk030621@users.noreply.github.com>
2025-06-09 23:00:14 +0100

    Added icon to Friendly Advice Dropdown

--- app/components/AuthButtons.js
+++ app/components/AuthButtons.js
@@ -34,7 +34,7 @@ export default function AuthButtons({ isAuthenticated, authorName }) {
       {loadingSignIn && !isAuthenticated ? (
         <div className="flex flex-col items-center justify-center min-h-[50px] mt-1">
           <p className="text-black text-lg mb-4">Signing in, please wait...</p>
-          <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-blue-700"></div>
+          <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-green-700"></div>
         </div>
       ) : loadingSignOut ? (
         /* Show loading screen when signing out */
@@ -88,6 +88,16 @@ export default function AuthButtons({ isAuthenticated, authorName }) {
                 <h2 className="text-xl font-bold text-gray-800 mb-3">
                   Join Me in Creating a “Nice Space” for Kindness and Connection
                 </h2>
+                <div className="w-full flex justify-center items-center py-6">
+                  <Image
+                    src="/lwj.jpg" // ✅ Replace with your actual image path
+                    alt="Tips & Tales - Share your wisdom and stories"
+                    width={90} // ✅ Adjust width as needed
+                    height={120} // ✅ Adjust height as needed
+                    unoptimized // ✅ Disables Next.js optimization
+                    className="rounded-lg shadow-2xl object-cover max-w-full h-auto "
+                  />
+                </div>
                 <p className="text-gray-600 leading-relaxed">
                   Hey everyone,
                   <br />
@@ -183,9 +193,18 @@ export default function AuthButtons({ isAuthenticated, authorName }) {
           <div className="mt-6">
             <button
               onClick={() => setShowExamples(!showExamples)}
-              className="px-4 py-2 bg-slate-600 hover:bg-slate-700 text-white rounded w-[310px]"
+              className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-full w-[310px] "
             >
-              {showExamples ? "Hide Guide" : "Guide"}
+              {showExamples ? "Hide Advice" : "Friendly Advice"}
+              <div className="justify-center items-center flex">
+                <Image
+                  src="/friendly advice.png"
+                  alt="tips icon"
+                  width={40}
+                  height={40}
+                  className="rounded-md ml-2"
+                />
+              </div>
             </button>
 
             {showExamples && (
